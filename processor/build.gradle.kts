c6e2a42c2d68eaf8f4e139b208a0eace36f4aade
Deming <aids61517@gmail.com>
2025-02-04 13:24:25 +0800

    upgrade library version

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -7,9 +7,9 @@ dependencies {
 
     implementation("com.google.devtools.ksp:symbol-processing-api:$kspVersion")
 
-    implementation("com.squareup:kotlinpoet:1.16.0")
-    implementation("com.squareup:kotlinpoet-ksp:1.16.0")
+    implementation("com.squareup:kotlinpoet:2.0.0")
+    implementation("com.squareup:kotlinpoet-ksp:2.0.0")
     implementation("com.squareup.okio:okio:3.9.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
 }
